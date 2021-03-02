c84b8af71f8d23c1f7f463225d2f47abaa0eabd3
Minsung Kim <minsungk@c5r5s3.42seoul.kr>
2021-03-02 13:44:59 +0900

    33Q my sol upload

--- test.c
+++ test.c
@@ -3,23 +3,21 @@
 int main()
 {
 	freopen("input.txt","rt",stdin);
-	int num, i, j, res;
+	int num, i, j, res, count;
+	int temp[100];
 	scanf("%d", &num);
-	int temp[101];
-
+	
 	i = 0;
 	while (i < num)
-	{
-		scanf("%d", &temp[i]);
-		i++;
-	}
+		scanf("%d", &temp[i++]);
+
 	i = 0;
 	while (temp[i] != '\0')
 	{
 		j = i + 1;
 		while (temp[j] != '\0')
 		{
-			if (temp[i] > temp[j])
+			if (temp[i] < temp[j])
 			{
 				res = temp[j];
 				temp[j] = temp[i];
@@ -29,11 +27,30 @@ int main()
 		}
 		i++;
 	}
+
 	i = 0;
-	while (temp[i])
+	count = 1;
+	while (temp[i] != '\0')
 	{
-		printf("%d ", temp[i]);
-		i++;
+		j = i + 1;
+		while (temp[j] != '\0')
+		{
+			if (temp[i] == temp[j])
+				j++;
+			else if (temp[i] > temp[j])
+			{
+				count++;
+				i++;
+				break;
+			}
+		}
+		if (count == 3)
+		{
+			res = temp[j];
+			break;
+		}
 	}
+	printf("%d", res);
 	return (0);
 }
+
