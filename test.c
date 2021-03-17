aa94271dd81fe0a462f6ebd026fe5c251c2a9fa9
MinsoftK <dolikart@naver.com>
2021-03-17 11:15:56 +0900

    43 upload

--- test.c
+++ test.c
@@ -1,44 +1,50 @@
 #include <stdio.h>
-#include <vector>
+#include <algorithm>
 using namespace std;
-int arr[1001];
 
-int Count(int s)//녹화하는데 필요한 DVD 개수를 리턴
-{
-	int i, cnt = 1, sum = 0;
-	for (i = 1; i <= n; i++)
+int arr[1000], num;
+
+int Count(int mid)	//DVD 장수를 반환하는 함
+{ 
+	int sum = 0, res, cnt = 1, i = 0;
+	while (i < num)
 	{
-		if (sum + arr[i] > s)
+		if (mid < (sum + arr[i]))
 		{
-			cnt++;
 			sum = arr[i];
+			cnt++;
 		}
-		else sum = sum + arr[i];
+		else
+		{
+			sum = sum + arr[i];
+		}
+		i++;
 	}
-	return (cnt);
+	return cnt;
 }
 
 int main()
 {
 	freopen("input.txt","rt",stdin);
-	int num, m, i, res, tmp, lt, rt, mid;
-	scanf("%d %d", &num, &m);
-	for (i = 1; i <= num; i++)
+	int key, i, lt, rt, mid, tmp = 0, res, max = -2147000000;
+	scanf("%d %d", &num, &key);
+	for (i = 0; i < num; i++)
 	{
 		scanf("%d", &arr[i]);
 		rt = rt + arr[i];
+		if (arr[i] > max) max = arr[i]; // 9 9 를 입력받을때 최대 길이는 9가 돼야하기 때문. 
 	}
-	sort(arr.begin(), arr.end());
+	lt = 0;
 	while (lt <= rt)
 	{
 		mid = (lt + rt) / 2;
-		if (Count(mid)) <= m)
+		if (mid >= max && Count(mid) <= key) //노래의 최대 길이보다 mid는 무조건 커야함. 
 		{
 			res = mid;
 			rt = mid - 1;
 		}
 		else lt = mid + 1;
 	}
-	printf("%d\n",res);
+	printf("%d\n", res);
 	return (0);
 }
