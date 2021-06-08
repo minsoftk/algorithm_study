cfc09acd98f71798466adcfbdaa84ff963806499
MinsoftK <dolikart@naver.com>
2021-06-08 16:11:07 +0900

    복면산 ing

--- test.c
+++ test.c
@@ -2,30 +2,12 @@
 #include <algorithm>
 using namespace std;
 
-int arr[1000], num;
-
-int Count(int mid)	//DVD 장수를 반환하는 함
-{ 
-	int sum = 0, res, cnt = 1, i = 0;
-	while (i < num)
-	{
-		if (mid < (sum + arr[i]))
-		{
-			sum = arr[i];
-			cnt++;
-		}
-		else
-		{
-			sum = sum + arr[i];
-		}
-		i++;
-	}
-	return cnt;
+int DFS(int L)
+{
 }
 
 int main()
 {
-	freopen("input.txt","rt",stdin);
-	
+	DFS(0);
 	return (0);
 }
