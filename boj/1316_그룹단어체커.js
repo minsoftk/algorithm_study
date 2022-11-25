306338d0597d8bfc701d4eb23502255a8fa33baf
minsoftk <minsoftk@gmail.com>
2022-11-25 18:19:10 +0900

    백준: 그룹 단어 체커

--- boj/1316_그룹단어체커.js
+++ boj/1316_그룹단어체커.js
@@ -1,17 +1,27 @@
 const filePath =
   process.platform === 'linux' ? '/dev/stdin' : './boj/input.txt';
-const input = require('fs').readFileSync(filePath).toString().trim();
+const [n, ...input] = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n');
 
 function solution(input) {
-  const croaRegex = [/c=/g, /c-/g, /dz=/g, /d-/g, /lj/g, /nj/g, /s=/g, /z=/g];
+  let res = 0;
+  input.forEach((item) => {
+    let len = item.split('').length;
+    let hash = new Map();
 
-  let inputString = input;
-
-  croaRegex.forEach((regex) => {
-    inputString = inputString.replace(regex, ' ');
+    let isGroup = true;
+    for (let i = 0; i < len; i += 1) {
+      // hash에 값이 들어가 있고 앞 뒤 문자가 같지 않다면 그룹이 아니다.
+      if (hash.get(item[i]) && i > 0 && item[i - 1] !== item[i])
+        isGroup = false;
+      hash.set(item[i], (hash.get(item[i]) || 0) + 1);
+    }
+    if (isGroup) res += 1;
   });
-
-  return inputString.length;
+  return res;
 }
 
 console.log(solution(input));
