73d563e45ba5c45705432337907204eab7fb7462
minsoftk <minsoftk@gmail.com>
2023-03-12 10:33:21 +0900

    [백준] 11718_그대로출력하기

---
+++ boj/11718_그대로출력하기.js
@@ -0,0 +1,13 @@
+const filePath =
+  process.platform === 'linux' ? '/dev/stdin' : './boj/input.txt';
+const input = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n');
+
+function solution() {
+  return input.join('\n');
+}
+
+console.log(solution());
