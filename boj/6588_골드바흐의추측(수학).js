f75db05ad84adb52964ed9c820b1b2530111e7c9
minsoftk <minsoftk@gmail.com>
2023-03-16 16:05:39 +0900

    [백준] 6588_골드바흐의추측 : 시간복잡도 최적화 dp 활용

--- boj/6588_골드바흐의추측(수학).js
+++ boj/6588_골드바흐의추측(수학).js
@@ -1,35 +1,74 @@
-const fs = require('fs');
 const filePath =
-	process.platform === 'linux' ? '/dev/stdin' : './boj/input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
+  process.platform === 'linux' ? '/dev/stdin' : './boj/input.txt';
+const input = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n');
 
-const WRONG = "Goldbach's conjecture is wrong.";
-let MAX = -1e9;
-let ans = 0;
-// console.log(input);
+input.pop();
 
-for (let i = 0; i < input.length - 1; i++) {
-	for (let j = input[i]; j >= 1; j--) {
-		let flag = true;
-		ans = 0;
+const dp = Array(1000000).fill(-1);
+dp[1] = 0;
+dp[2] = 0;
 
-		for (let k = 2; k * k <= input[i]; k++) {
-			if (j <= 1 || input[i] - j <= 1) {
-				flag = false;
-			}
+function solution() {
+  const WRONG = "Goldbach's conjecture is wrong.";
 
-			// input[i] - j 에서는 k와 같은 경우가 존재하기 때문에 해당 경우를 생각해줘서 작성
-			if (j % k === 0 || (input[i] - j > k && (input[i] - j) % k === 0)) {
-				flag = false;
-			}
-		}
-
-		if (flag) {
-			ans = Math.max(ans, j);
-			break;
-		}
-	}
-
-	if (ans < 0) console.log(WRONG);
-	else console.log(`${input[i]} = ${input[i] - ans} + ${ans}`);
+  return input
+    .map((elem) => {
+      const num = +elem;
+      for (let b = num; b >= num / 2; b -= 1) {
+        const a = num - b;
+        if (isOddPrime(b) && isOddPrime(a)) {
+          return `${num} = ${a} + ${b}`;
+        }
+      }
+      return WRONG;
+    })
+    .join('\n');
 }
+
+function isOddPrime(num) {
+  if (num % 2 === 0) {
+    dp[num] = 0;
+    return false;
+  }
+  if (dp[num] >= 0) return dp[num];
+
+  for (let i = 2; i * i <= num; i += 1) {
+    if (num % i === 0) {
+      dp[num] = 0;
+      return false;
+    }
+  }
+  dp[num] = 1;
+  return true;
+}
+
+console.log(solution());
+
+// for (let i = 0; i < input.length - 1; i++) {
+// 	for (let j = input[i]; j >= 1; j--) {
+// 		let flag = true;
+// 		ans = 0;
+
+// 		for (let k = 2; k * k <= input[i]; k++) {
+// 			if (j <= 1 || input[i] - j <= 1) {
+// 				flag = false;
+// 			}
+
+// 			// input[i] - j 에서는 k와 같은 경우가 존재하기 때문에 해당 경우를 생각해줘서 작성
+// 			if (j % k === 0 || (input[i] - j > k && (input[i] - j) % k === 0)) {
+// 				flag = false;
+// 			}
+// 		}
+
+// 		if (flag) {
+// 			ans = Math.max(ans, j);
+// 			break;
+// 		}
+// 	}
+
+// 	if (ans < 0) console.log(WRONG);
+// 	else console.log(`${input[i]} = ${input[i] - ans} + ${ans}`);
