1ae6f8cbc3fc67d893d5b2039e830d5b24f8ef3d
minsoftk <minsoftk@gmail.com>
2023-03-16 14:49:44 +0900

    [백준] 14426_접두사찾기

--- boj/14426_접두사찾기.js
+++ boj/14426_접두사찾기.js
@@ -8,24 +8,25 @@ const [first, ...last] = require('fs')
 
 const [n, m] = first.trim().split(' ').map(Number);
 
-let arr = last.slice();
-
 function solution() {
-  // let check = Array(m).fill(0);
-  // for (let i = 0; i < S.length; i++) {
-  //   for (let j = 0; j < checkstr.length; j++) {
-  //     let temp = S[i].substr(0, checkstr[j].length);
-  //     if (check[j] > 0) continue;
-  //     if (temp === checkstr[j]) {
-  //       check[j] = 1;
-  //     }
-  //   }
-  // }
-  // let cnt = 0;
-  // for (let x of check) {
-  //   if (x === 1) cnt += 1;
-  // }
-  // return cnt;
+  const [mainString, checkString] = [
+    last.slice(0, n),
+    last.slice(n, last.length),
+  ];
+
+  let hash = new Map();
+  mainString.forEach((elem) => {
+    for (let i = 1; i <= elem.length; i += 1) {
+      hash.set(elem.slice(0, i), 1);
+    }
+  });
+
+  let cnt = 0;
+  checkString.forEach((elem) => {
+    const hashVal = hash.get(elem);
+    if (hashVal) cnt += 1;
+  });
+  return cnt;
 }
 
-console.log(solution(n, m, S, checkstr));
+console.log(solution());
