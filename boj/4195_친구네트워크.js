e2246881bebfef6bd84f308b69304ecc272943da
minsoftk <minsoftk@gmail.com>
2023-10-25 18:00:52 +0900

    [백준] 4195_친구네트워크

--- boj/4195_친구네트워크.js
+++ boj/4195_친구네트워크.js
@@ -7,32 +7,55 @@ const [n, ...input] = require('fs')
   .split('\n');
 
 function solution(input) {
+  function find(x) {
+    if (x === parent[x]) return x;
+    else {
+      return (parent[x] = find(parent[x]));
+    }
+  }
+
+  function union(x, y) {
+    x = find(x);
+    y = find(y);
+
+    if (x !== y) {
+      parent[y] = x;
+      number[x] += number[y];
+    }
+  }
+
+  let parent = {};
+  let number = {};
   let idx = 0;
   let answer = [];
+
   for (let i = 0; i < n; i += 1) {
     const cnt = Number(input[idx++]);
+    parent = {};
+    number = {};
     let arr = [];
     for (let j = 0; j < cnt; j += 1) {
       arr.push(input[j + idx]);
     }
     idx += cnt;
 
-    console.log('arr:', arr);
-    let first = arr[0].split(' ')[0];
-    let set = new Set();
-    set.add(first);
-    let hash = new Map();
-    for (let i = 0; i ) {
-      const [a, b] = elem.split(' ');
-      hash.set(a, b);
-      hash.set(b, a);
-      set.add(a);
-      set.add(b);
-      // console.log('hash:', hash);
-      answer.push(set.size);
-    }
+    arr = arr.map((a) => a.split(' '));
+    arr.forEach((elem) => {
+      const [a, b] = elem;
+      if (!parent.hasOwnProperty(a)) {
+        parent[a] = a;
+        number[a] = 1;
+      }
+      if (!parent.hasOwnProperty(b)) {
+        parent[b] = b;
+        number[b] = 1;
+      }
+      union(a, b);
+      answer.push(number[find(a)]);
+    });
   }
-  // console.log('answer:', answer);
+
+  return answer.join('\n');
 }
 
 console.log(solution(input));
