4356c45fb54c40686c3e87c0461d2cceddaa7fdf
minsoftk <minsoftk@gmail.com>
2023-10-31 18:08:45 +0900

    [백준] 1374_강의실: 우선순위 큐

---
+++ boj/1374_강의실.js
@@ -0,0 +1,151 @@
+const filePath =
+  process.platform === 'linux' ? '/dev/stdin' : './boj/input.txt';
+const [n, ...input] = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n');
+
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  // Helper Methods
+  getLeftChildIndex(parentIndex) {
+    return 2 * parentIndex + 1;
+  }
+  getRightChildIndex(parentIndex) {
+    return 2 * parentIndex + 2;
+  }
+
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+
+  hasLeftChild(index) {
+    return this.getLeftChildIndex(index) < this.heap.length;
+  }
+
+  hasRightChild(index) {
+    return this.getRightChildIndex(index) < this.heap.length;
+  }
+
+  hasParent(index) {
+    return this.getParentIndex(index) >= 0;
+  }
+
+  leftChild(index) {
+    return this.heap[this.getLeftChildIndex(index)];
+  }
+
+  rightChild(index) {
+    return this.heap[this.getRightChildIndex(index)];
+  }
+
+  parent(index) {
+    return this.heap[this.getParentIndex(index)];
+  }
+
+  swap(indexOne, indexTwo) {
+    const temp = this.heap[indexOne];
+    this.heap[indexOne] = this.heap[indexTwo];
+    this.heap[indexTwo] = temp;
+  }
+
+  peek() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+    return this.heap[0];
+  }
+
+  // Removing an element will remove the
+  // top element with highest priority then
+  // heapifyDown will be called
+  remove() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+    const item = this.heap[0];
+    this.heap[0] = this.heap[this.heap.length - 1];
+    this.heap.pop();
+    this.heapifyDown();
+    return item;
+  }
+
+  add(item) {
+    this.heap.push(item);
+    this.heapifyUp();
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (this.hasParent(index) && this.parent(index) > this.heap[index]) {
+      this.swap(this.getParentIndex(index), index);
+      index = this.getParentIndex(index);
+    }
+  }
+
+  heapifyDown() {
+    let index = 0;
+    while (this.hasLeftChild(index)) {
+      let largerChildIndex = this.getLeftChildIndex(index);
+      if (
+        this.hasRightChild(index) &&
+        this.rightChild(index) <= this.leftChild(index)
+      ) {
+        largerChildIndex = this.getRightChildIndex(index);
+      }
+      if (this.heap[index] <= this.heap[largerChildIndex]) {
+        break;
+      } else {
+        this.swap(index, largerChildIndex);
+      }
+      index = largerChildIndex;
+    }
+  }
+
+  printHeap() {
+    var heap = ` ${this.heap[0]} `;
+    for (var i = 1; i < this.heap.length; i++) {
+      heap += ` ${this.heap[i]} `;
+    }
+    console.log(heap);
+  }
+}
+
+function solution(input) {
+  let arrs = input.map((elem) => elem.split(' ').map(Number));
+  let answer = 0;
+
+  const heap = new MinHeap();
+
+  arrs.sort((a, b) => b[1] - a[1]);
+
+  // push end time
+
+  heap.add(arrs.pop()[2]);
+  answer = 1;
+
+  for (let i = 0; i < n - 1; i += 1) {
+    // 강의 목록에서 새 강의 꺼내기
+    const [newId, newStartTime, newEndTime] = arrs.pop();
+
+    // 기존 강의에서 가장 일찍 끝나는 강의
+    end = heap.remove();
+
+    // 강의 시간이 겹치면
+    if (newStartTime < end) {
+      heap.add(end);
+      heap.add(newEndTime);
+      answer += 1;
+    } else {
+      heap.add(newEndTime);
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution(input));
