f05dfd1e07d019b5e85cb463f66b0c0262c650c5
minsoftk <minsoftk@gmail.com>
2023-11-01 17:11:30 +0900

    [백준] 11286_절댓값힙

--- boj/11286_절댓값힙.js
+++ boj/11286_절댓값힙.js
@@ -7,17 +7,21 @@ const [n, ...input] = require('fs')
   .split('\n')
   .map(Number);
 
+// 틀린거
 class MinHeap {
   constructor() {
     this.heap = [];
   }
 
-  // Helper Methods
+  // 1. idx를 가지고 와야됨
+  // 2. 노드가 존재하는지 확인해야 됨.
+  // 3. 깂을 가져와야됨
+
   getLeftChildIndex(parentIndex) {
-    return 2 * parentIndex + 1;
+    return parentIndex * 2 + 1;
   }
   getRightChildIndex(parentIndex) {
-    return 2 * parentIndex + 2;
+    return parentIndex * 2 + 2;
   }
   getParentIndex(childIndex) {
     return Math.floor((childIndex - 1) / 2);
@@ -40,105 +44,97 @@ class MinHeap {
   parent(index) {
     return this.heap[this.getParentIndex(index)];
   }
-
-  // Functions to create Min Heap
-
-  swap(indexOne, indexTwo) {
-    const temp = this.heap[indexOne];
-    this.heap[indexOne] = this.heap[indexTwo];
-    this.heap[indexTwo] = temp;
-  }
-
-  peek() {
-    if (this.heap.length === 0) {
-      return null;
-    }
-    return this.heap[0];
-  }
-
-  // Removing an element will remove the
-  // top element with highest priority then
-  // heapifyDown will be called
-  remove() {
-    if (this.heap.length === 0) {
-      return null;
-    }
-    const item = this.heap[0];
-    this.heap[0] = this.heap[this.heap.length - 1];
-    this.heap.pop();
-    this.heapifyDown();
-    return item;
-  }
-
-  add(item) {
-    this.heap.push(item);
-    this.heapifyUp();
+  swap(index1, index2) {
+    const temp = this.heap[index1];
+    this.heap[index1] = this.heap[index2];
+    this.heap[index2] = temp;
   }
 
   heapifyUp() {
-    let index = this.heap.length - 1;
-    while (this.hasParent(index) && this.parent(index) > this.heap[index]) {
-      this.swap(this.getParentIndex(index), index);
-      index = this.getParentIndex(index);
+    let idx = this.heap.length - 1;
+    while (this.hasParent(idx) && this.parent(idx)[0] > this.heap[idx][0]) {
+      this.swap(this.getParentIndex(idx), idx);
+      idx = this.getParentIndex(idx);
     }
   }
 
+  insert(data) {
+    this.heap.push(data);
+    this.heapifyUp();
+  }
+
   heapifyDown() {
-    let index = 0;
-    while (this.hasLeftChild(index)) {
-      let smallerChildIndex = this.getLeftChildIndex(index);
+    let idx = 0;
+    while (this.hasLeftChild(idx)) {
+      let smallerChildIndex = this.getLeftChildIndex(idx);
       if (
-        this.hasRightChild(index) &&
-        this.rightChild(index) < this.leftChild(index)
+        this.hasRightChild(idx) &&
+        this.rightChild(idx)[0] < this.leftChild(idx)[0]
       ) {
-        smallerChildIndex = this.getRightChildIndex(index);
+        // smallerChildIndex = this.getLeftChildIndex(idx);
+        smallerChildIndex = this.getRightChildIndex(idx);
       }
-      if (this.heap[index] < this.heap[smallerChildIndex]) {
+      if (this.heap[idx][0] < this.heap[smallerChildIndex][0]) {
         break;
       } else {
-        this.swap(index, smallerChildIndex);
+        this.swap(idx, smallerChildIndex);
       }
-      index = smallerChildIndex;
+      idx = smallerChildIndex;
     }
+  }
+
+  pop() {
+    if (this.heap.length === 0) return null;
+
+    let result = this.heap[0];
+    this.heap[0] = this.heap[this.heap.length - 1];
+    this.heap.pop();
+    this.heapifyDown();
+    return result;
+  }
+
+  peek() {
+    if (this.heap.length === 0) return null;
+    return this.heap[0];
   }
 
   size() {
     return this.heap.length;
   }
 
-  printHeap() {
-    var heap = ` ${this.heap[0]} `;
-    for (var i = 1; i < this.heap.length; i++) {
-      heap += ` ${this.heap[i]} `;
+  print() {
+    for (let i = 0; i < this.heap.length; i++) {
+      console.log(this.heap[i]);
     }
-    console.log(heap);
   }
 }
 
 function solution(input) {
-  // 0이 아닌 경우 queue에 입력
-  // 절대값이 같은 경우 작은 값을 출력
-  // 배열이 비어있을 때 0이면 0을 출력
-  let heap = new MinHeap();
-  let hash = new Map();
+  let answer = [];
+  const minHeap = new MinHeap();
 
-  input.forEach((x, idx) => {
-    hash.set(idx, [x, Math.abs(x)]);
-  });
+  for (let i = 0; i < input.length; i++) {
+    const num = input[i];
 
-  let result = [];
-  input.forEach((x, idx) => {
-    if (x === 0) {
-      if (heap.size() === 0) result.push(0);
-      else {
-        const [absOrigin, origin] = heap.remove();
-        result.push(origin);
-      }
+    if (num !== 0) {
+      minHeap.insert([Math.abs(num), num]);
     } else {
-      heap.add([Math.abs(x), x]);
+      if (minHeap.size() === 0) answer.push(0);
+      else if (minHeap.size() === 1) answer.push(minHeap.pop()[1]);
+      else {
+        let top = minHeap.pop();
+        const temp = [top];
+        while (minHeap.size() > 0 && top[0] === minHeap.peek()[0]) {
+          temp.push(minHeap.pop());
+        }
+
+        temp.sort((a, b) => b[1] - a[1]);
+        answer.push(temp.pop()[1]);
+        temp.forEach((elem) => minHeap.insert(elem));
+      }
     }
-  });
-  return result.join('\n');
+  }
+  return answer.join('\n');
 }
 
 console.log(solution(input));
