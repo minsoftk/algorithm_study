b5445a7982a555caa2ee6b686cdce4f74e3ee58a
minsoftk <minsoftk@gmail.com>
2023-11-10 15:55:53 +0900

    [알고리즘] 병합정렬 새로운 방법

--- 알고리즘/정렬/MergeSort.js
+++ 알고리즘/정렬/MergeSort.js
@@ -41,3 +41,42 @@ function MergeSort(arr) {
 }
 
 console.log(MergeSort(arr));
+
+/**
+ * 각각의 split 된 배열을 직접 나열해주는 함수. 위의 함수는 한단계 더 추상화가 되어있다.
+ * @param {} arr
+ * @returns
+ */
+function merge_sort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const left = merge_sort(arr.slice(0, mid));
+  const right = merge_sort(arr.slice(mid, arr.length));
+
+  let leftIdx = 0,
+    rightIdx = 0,
+    idx = 0;
+  while (leftIdx < left.length && rightIdx < right.length) {
+    if (left[leftIdx] < right[rightIdx]) {
+      arr[idx] = left[leftIdx++];
+    } else {
+      arr[idx] = right[rightIdx++];
+    }
+    idx += 1;
+  }
+  if (leftIdx === left.length) {
+    while (rightIdx < right.length) {
+      arr[idx] = right[rightIdx++];
+      idx += 1;
+    }
+  } else if (rightIdx === right.length) {
+    while (leftIdx < left.length) {
+      arr[idx] = left[leftIdx++];
+      idx += 1;
+    }
+  }
+  return arr;
+}
+
+console.log(merge_sort(arr));
