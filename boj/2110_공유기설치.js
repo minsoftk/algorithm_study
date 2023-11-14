55a5ab10b5a368e39852528a3199f1dc7296434b
minsoftk <minsoftk@gmail.com>
2023-11-14 16:11:30 +0900

    [백준] 2110_공유기설치 : 이진탐색

--- boj/2110_공유기설치.js
+++ boj/2110_공유기설치.js
@@ -1,43 +1,42 @@
-const { count } = require('console');
-const fs = require('fs');
-const filePath =
-	process.platform === 'linux' ? '/dev/stdin' : './boj/input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n');
-let [n, m] = input[0].trim().split(' ').map(Number);
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '/input.txt';
+const input = require('fs')
+  .readFileSync(__dirname + filePath)
+  .toString()
+  .trim()
+  .split('\n');
+let [n, c] = input[0].trim().split(' ').map(Number);
 let arr = [];
 for (let i = 1; i < input.length; i++) {
-	arr.push(Number(input[i].trim()));
+  arr.push(Number(input[i].trim()));
 }
 
-function solution(n, m, arr) {
-	function count(){
+function solution(n, c, arr) {
+  arr.sort((a, b) => a - b);
 
-	}
-// 0 3
+  let start = 1;
+  let end = arr[arr.length - 1];
+  let result = 0;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    let value = arr[0];
+    let cnt = 1;
 
-	let answer = 1e9;
-	let check = Array(n).fill(0);
-	arr.sort((a, b) => a - b);
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] >= value + mid) {
+        value = arr[i];
+        cnt += 1;
+      }
+    }
 
-	// 거리를 기준으로 이분 탐색을 진행
-	// 가능한 최대 거리가 right에 저장
-	let left = 1,
-		right = arr[n - 1] - arr[0];
-	check[left] = 1;
+    // m개 이상의 공유기를 설치할 수 있는 경우
+    if (cnt >= c) {
+      start = mid + 1;
+      result = mid;
+    } else {
+      end = mid - 1;
+    }
+  }
 
-	while (left <= right) {
-		let mid = parseInt((left + right) / 2);
-		let start = arr[0];
-
-		for (let i = 0; i < n; i++) {
-			let d = arr[i] - start;
-			if (count(mid) >= m) {
-				answer = mid;
-				left = mid + 1;
-			} else right = mid - 1;
-		}
-	}
-
-	return answer;
+  return result;
 }
-console.log(solution(n, m, arr)); // 200
+console.log(solution(n, c, arr)); // 200
