78b39c396ea535c817034ccc3b7295ef71aed331
minsoftk <minsoftk@gmail.com>
2023-11-29 17:47:18 +0900

    temp commit 1939

--- boj/1939_중량제한.js
+++ boj/1939_중량제한.js
@@ -9,21 +9,48 @@ function solution(input) {
   let [n, m] = input[0].trim().split(' ').map(Number);
   let graph = Array.from({ length: n + 1 }, () => Array());
 
-  let start = 1;
-  let end = -1;
+  let startWeight = 1e9,
+    endWeight = 1;
   for (let i = 1; i <= m; i += 1) {
     const [a, b, bridge] = input[i].split(' ').map(Number);
     graph[a].push([b, bridge]);
     graph[b].push([a, bridge]);
-    if (end < bridge) end = bridge;
+    startWeight = Math.min(startWeight, bridge);
+    endWeight = Math.max(endWeight, bridge);
   }
-  const [a, b] = input[m + 1].split(' ').map(Number);
-  console.log(graph);
+  const [startNode, endNode] = input[m + 1].split(' ').map(Number);
 
-  while (start <= end) {
-    const mid = Math.floor((start + end) / 2);
+  result = startWeight;
+  while (startWeight <= endWeight) {
+    const mid = Math.floor((startWeight + endWeight) / 2);
+    console.log('bfs(mid):', bfs(mid));
+    if (bfs(mid)) {
+      result = mid;
+      startWeight = mid + 1;
+    } else end = mid - 1;
   }
 
-  return 0;
+  return result;
+
+  function bfs(inputWeight) {
+    const queue = [...graph[startNode]];
+    console.log('queue:', queue);
+    const check = Array(n + 1).fill(false);
+    check[startNode] = true;
+
+    while (queue.length) {
+      console.log('check:', check);
+      const [idx, weight] = queue.shift();
+      console.log('graph[idx]:', idx, graph[idx]);
+      for (let [nextIdx, nextWeight] of graph[idx]) {
+        if (!check[nextIdx] && nextWeight >= inputWeight) {
+          check[nextIdx] = true;
+          queue.push([nextIdx, nextWeight]);
+        }
+      }
+    }
+    return check[endNode];
+  }
 }
+
 console.log(solution(input)); // 200
