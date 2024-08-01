1eae141b74d71d455346cd964b77e16c68cb81fd
minsoftk <minsoftk@gmail.com>
2024-08-01 17:35:07 +0900

    leet: 1.TwoSum - py 코드 수정

--- leetcode/1.TwoSum.py
+++ leetcode/1.TwoSum.py
@@ -1,11 +1,9 @@
 def twoSum(nums, target):
     dics = {nums[x]:x for x in range(0,len(nums))}
-    
-    for i in range(0, len(nums)):
-        findVal = target - nums[i]
-        if findVal in dics.keys() and i != dics[findVal]:
-            return [i, dics[findVal]]
-    
+    for i, val in enumerate(nums):
+        find_value = target - val
+        if find_value in dics.keys() and i != dics[find_value]:
+            return [i, dics[find_value]]
 
 print(twoSum([2,7,11,15],9))
 print(twoSum([3,2,4],6))
