12c4ade9aa3d75912b161d07f4ff20c4321e0108
minsoftk <minsoftk@gmail.com>
2024-08-27 11:10:22 +0900

    conf: 필요없는 파일 정리

--- leetcode/easy-collection(featured)/String/Reverse Integer.js
+++
@@ -1,8 +0,0 @@
-var reverse = function (x) {
-	let flag = 1;
-	if (x < 0) flag = -1;
-	x = x.toString().split('').reverse().join('');
-	x = parseInt(x);
-	if (x > -1 * Math.pow(2, 31) && x > Math.pow(2, 31)) return 0;
-	return x * flag;
-};
