787a80502821659ecf64a00fe9a73ecad5c7683e
minsoftk <minsoftk@gmail.com>
2025-01-09 00:46:06 +0900

    boj(dp): 11053_가장긴증가하는부분수열(LIS)

--- boj/11053_가장긴증가하는부분수열.js
+++ boj/11053_가장긴증가하는부분수열.js
@@ -1,9 +1,5 @@
-const { count, timeStamp } = require('console');
-const fs = require('fs');
-const { DefaultSerializer } = require('v8');
-const filePath =
-	process.platform === 'linux' ? '/dev/stdin' : './boj/input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+let input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 let n = parseInt(input[0].trim());
 let arr = '';
 for (let i = 1; i < input.length; i++) {
@@ -13,26 +9,48 @@ arr = arr.split(',').map(Number);
 // console.log(m, n, k, arr);
 // console.log(n, arr);
 
-function solution(n, arr) {
-	let answer = 0;
+// function solution(n, arr) {
+// 	let answer = 0;
 
-	// dy : i번까지의 연속된 수열의 개수를 저장한다.
-	let dy = Array(n).fill(0);
+// 	// dy : i번까지의 연속된 수열의 개수를 저장한다.
+// 	let dy = Array(n).fill(0);
 
-	// 1번 idx부터 탐색
-	dy[0] = 1;
-	for (let i = 1; i < n; i++) {
+// 	// 1번 idx부터 탐색
+// 	dy[0] = 1;
+// 	for (let i = 1; i < n; i++) {
+// 		let max = 0;
+// 		// 이전의 dy값중 최대값을 찾는다.
+// 		for (let j = i - 1; j >= 0; j--) {
+// 			if (arr[j] < arr[i]) max = Math.max(dy[j], max);
+// 		}
+// 		// 최대값에서 + 1을 해준다.
+// 		dy[i] = max + 1;
+// 	}
+// 	// 최대값을 저장한다.
+// 	answer = Math.max(...dy);
+// 	return answer;
+// }
+
+function solution2(n, arr) {
+	let dp = Array(n + 1)
+		.fill(0)
+		.map(() => -1);
+
+	arr = [0, ...arr];
+	dp[1] = 1;
+
+	for (let i = 2; i < n + 1; i += 1) {
 		let max = 0;
-		// 이전의 dy값중 최대값을 찾는다.
-		for (let j = i - 1; j >= 0; j--) {
-			if (arr[j] < arr[i]) max = Math.max(dy[j], max);
+		for (let j = 1; j < i; j += 1) {
+			if (arr[j] < arr[i]) {
+				max = Math.max(max, dp[j]);
+			}
 		}
-		// 최대값에서 + 1을 해준다.
-		dy[i] = max + 1;
+		dp[i] = max + 1;
 	}
-	// 최대값을 저장한다.
-	answer = Math.max(...dy);
-	return answer;
+
+	return Math.max(...dp);
 }
-console.log(solution(n, arr));
+
+console.log(solution2(n, arr));
 // 4
