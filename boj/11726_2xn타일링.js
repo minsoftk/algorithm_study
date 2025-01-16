de68506a221ff1d5ef9229df5fe3c3a4d3671532
minsoftk <minsoftk@gmail.com>
2025-01-16 11:32:54 +0900

    boj(dp): 11726_2xn타일링 2번째 풀이
    - dp의 저장되는 값의 의미를 잘 생각해보면 됐는데, 너무 어렵게 생각했다.

--- boj/11726_2xn타일링.js
+++ boj/11726_2xn타일링.js
@@ -1,19 +1,15 @@
-const filePath =
-  process.platform === 'linux' ? '/dev/stdin' : './boj/input.txt';
-const input = require('fs')
-  .readFileSync(filePath)
-  .toString()
-  .trim()
-  .split('\n');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 
 function solution(n) {
-  const arr = Array(n).fill(0);
-  [arr[1], arr[2]] = [1, 2];
+	const dp = Array(n + 1).fill(0);
+	[dp[1], dp[2]] = [1, 2];
 
-  for (let i = 3; i <= n; i += 1) {
-    arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
-  }
-  return arr[n] % 10007;
+	for (let i = 3; i <= n; i += 1) {
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+	}
+
+	return dp[n];
 }
 
 console.log(solution(+input)); // 2
