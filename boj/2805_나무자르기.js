ae6bd92f1eb937c3a848abcbef8abd0d82268927
minsoftk <minsoftk@gmail.com>
2025-05-07 15:49:46 +0900

    boj(파라메트릭서치): 2805_나무자르기
    - 파라메트릭 서치 예제(이분탐색 응용)

--- boj/2805_나무자르기.js
+++ boj/2805_나무자르기.js
@@ -1,10 +1,5 @@
-const filePath =
-	process.platform === 'linux' ? '/dev/stdin' : './boj/input.txt';
-const input = require('fs')
-	.readFileSync(filePath)
-	.toString()
-	.trim()
-	.split('\n');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 
 let n = Number(input[0].split(' ')[0]);
 let m = Number(input[0].split(' ')[1]);
@@ -50,6 +45,7 @@ function count(arr, mid) {
 	}
 	return sum;
 }
+
 function solution(n, m, arr) {
 	let left = 0,
 		right = 20e10;
@@ -66,4 +62,63 @@ function solution(n, m, arr) {
 	return answer;
 }
 
-console.log(solution(n, m, arr));
+// console.log(solution(n, m, arr));
+
+/** 2025/05/07 재풀이 */
+function solution2(n, m, arr) {
+	arr.sort((a, b) => a - b);
+
+	let left = 0;
+	let right = arr[n - 1];
+	let answer = 0;
+
+	while (left <= right) {
+		const mid = Math.floor((left + right) / 2);
+		if (isPossible(mid) >= m) {
+			left = mid + 1;
+			answer = Math.max(answer, mid);
+		} else right = mid - 1;
+	}
+
+	return answer;
+}
+
+function isPossible(h) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i += 1) {
+		if (arr[i] > h) {
+			sum += arr[i] - h;
+		}
+	}
+
+	return sum;
+}
+
+function solution3(n, m, arr) {
+	arr.sort((a, b) => a - b);
+	let cur = -1;
+	let step = arr.length - 1;
+
+	while (step !== 0) {
+		while (cur + step <= 1e9 && isPossible2(cur + step)) {
+			cur += step;
+		}
+		step = Math.floor(step / 2);
+	}
+
+	return cur;
+}
+
+function isPossible2(h) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i += 1) {
+		if (arr[i] > h) {
+			sum += arr[i] - h;
+		}
+	}
+
+	return sum >= m;
+}
+
+console.log(solution2(n, m, arr));
+console.log(solution3(n, m, arr));
